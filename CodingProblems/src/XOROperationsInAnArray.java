fb6b798f30c71d252eb6770646c61a8c540f6c42
bal2838 <bale2838@gmail.com>
2021-06-14 00:40:46 -0500

    Add files via upload

---
+++ CodingProblems/src/XOROperationsInAnArray.java
@@ -0,0 +1,18 @@
+/*
+Given an integer n and an integer start.
+
+Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+
+Return the bitwise XOR of all elements of nums.
+ */
+public class XOROperationsInAnArray {
+	public int xorOperation(int n, int start) {
+		int[] nums = new int[n];
+		int xor = 0;
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = start + 2*i;
+			xor ^= nums[i];
+		}
+		return xor;
+	}
+}
